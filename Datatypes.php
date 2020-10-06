c4e6d8a039f672d57d475e3f9f0852a669c63d66
Chandlz20057 <camchandlz@outlook.com>
2020-10-06 20:41:45 +0100

    Datatypes.php

---
+++ Datatypes.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+<body>
+<?php
+$string = "string datatype";
+$boolean = [false] ; 
+$integer = 13;
+$decimals = 10.5;
+echo "Data-types are defined by the values it can take, the programming language used, or the operations that can be performed on it.";
+echo "When using a $string you need to use the inverted commas (Speech marks).";
+echo "A boolean data-type tell the computer or user whether the infomation being given is false or true.";
+echo "A string data-type is used to show either a number or a decimal for example integer data-types will show $integer and Decimal data-types will show $decimals.";
+?>
+</body>
+</head>
+</html>
