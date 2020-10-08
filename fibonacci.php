e3076da7c39d13f1610fc56cc1d7b96c0b12283c
Chandlz20057 <camchandlz@outlook.com>
2020-10-08 21:19:25 +0100

    Create fibonacci.php

---
+++ fibonacci.php
@@ -0,0 +1,12 @@
+<?php
+
+function fibonacci(){
+    $num1 = 1;
+    $num2 = 1;
+
+    for ($i=0; $i <= 12;){
+        $num1 = $num1 . $num2;
+        $num2 = prev($num1);
+    }
+    echo "$num1";
+}
