ae7a14a669315a7ef8abbb06dba92d6a07146751
Chandlz20057 <camchandlz@outlook.com>
2020-10-08 20:20:26 +0100

    Create reversefunction.php

---
+++ reversefunction.php
@@ -0,0 +1,18 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<body>
+<?php
+function reverse_string($string){
+strlen($string);
+for ($i = 0; $i < strlen($string); $i++); {
+        $letter = $string[$i];
+             
+}
+}
+echo "$string";
+
+?>
+</body>
+</head>
+</html>
