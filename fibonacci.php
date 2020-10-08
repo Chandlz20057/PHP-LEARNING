a8e750bb5451cdd37d9a63ba2dcce78d67c44656
Chandlz20057 <camchandlz@outlook.com>
2020-10-08 21:25:48 +0100

    Update fibonacci.php

--- fibonacci.php
+++ fibonacci.php
@@ -1,11 +1,11 @@
 <?php
 
 function fibonacci(){
-    $num1 = 1;
+    $num1 = 0;
     $num2 = 1;
 
     for ($i=0; $i <= 12;){
-        $num1 = $num1 . $num2;
+        $num1 = $num1 + $num2;
         $num2 = prev($num1);
     }
     echo "$num1";
