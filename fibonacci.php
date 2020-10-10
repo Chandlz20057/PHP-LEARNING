4985038efc13aefe161584c7793a4e8f188a463c
Chandlz20057 <camchandlz@outlook.com>
2020-10-10 12:10:09 +0100

    Update fibonacci.php

--- fibonacci.php
+++ fibonacci.php
@@ -1,16 +1,18 @@
 <?php
-function fibonacci(){
+
+function fibonacci($until) {
     $num1 = 0;
     $num2 = 1;
+    
+    for ($i = 0; $i <= $until; $i++) {
+        if ($num1 === 0) {
+            echo '1 <br />';
+        }
 
-    for ($i=0; $i <= 12; $i++){
         $num3 = $num1 + $num2;
-
-        echo $num3;
-     
         $num1 = $num2;
         $num2 = $num3;
     }
 }
 
-fibonacci();
+fibonacci(12);
