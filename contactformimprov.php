57f0300c3797c958cfa5793066791cfd83ffbe1b
Chandlz20057 <camchandlz@outlook.com>
2020-10-13 20:32:29 +0100

    Update contactformimprov.php

--- contactformimprov.php
+++ contactformimprov.php
@@ -10,26 +10,20 @@ echo "<h1>Contact Form.<br></h1>";
 ?>
 <form action="contactformimprov.php" method="GET">
         Name: <br><input type="text" name="Name"><br>
-        Age: <br><input type="string" name="Age"><br>
+        Age: <br><input type="number" name="Age"><br>
         Email: <br><input type="email" name="Email"><br>
         <input type="submit" name="submit" value="submit">
    </form>
 
 <?php
-
-if  (isset( $_GET['submit'] )) {
-    echo "<u><h3>This is your Name:</h3></u>";
-    echo $_GET ["Name"];
-    echo "<br>";
-    echo "<u><h3>This is your current age:</h3></u>";
-    echo $_GET ["Age"];
-    echo "<br>";
-    echo "<u><h3>This is your Email:</h3></u>";
-    echo $_GET ["Email"];
-    
-
-}
-
+    $myfile = fopen("submission.txt", "w", "a") or die("Unable to open file!");
+    $txt = "Name\n";
+    fwrite($myfile, $txt);
+    $txt = "Age\n";
+    fwrite($myfile, $txt);
+    $txt = "Email\n";
+    fwrite($myfile, $txt);
+    fclose($myfile);
 ?>
 
 </body>
