347a0827f11a2be8c5a99a894a1b70e9d535d251
Chandlz20057 <camchandlz@outlook.com>
2020-10-19 20:54:29 +0100

    Update abstractclasses.php

--- abstractclasses.php
+++ abstractclasses.php
@@ -5,23 +5,27 @@ abstract class Karate {
     $this->name = $name;
   }
 }
-
-class Blocks extends Karate {
-    public function __construct($name);
-        $this->name;
-  }
+class Kata extends Karate {
+    
 }
 
-class Kata extends Karate {
-    public function __construct($name);
-        $this->name!;
-  }
+class Blocks extends Karate {
+
 }
 
 class Basics extends Karate {
-    public function __construct($name);
-        $this->name;
-  }
+
 }
 
+echo "<h1> <u> <b>Private  </b></u> </h1>";
+echo "<br />";
+echo "<h1>Private classes means people can only read.Also it can only be accessed from within the class that defines it</h1>";
+echo "<br />";
+echo "<h1> <u><b>Protected  </b></u> </h1>";
+echo "<br />";
+echo "<h1>Protected classes can only be accessed from within the class that defines it, or a descendant of that class</h1>";
+echo "<br />";
+echo "<h1> <u><b>Public </b> </u> </h1>";
+echo "<br />";
+echo "<h1>Public classes can be writen and read. (Can be accessed from anywhere)</h1>";
 ?>
