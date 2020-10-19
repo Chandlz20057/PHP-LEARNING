d02abb21c77931d09e70ceb66ed7660838d49b93
Chandlz20057 <camchandlz@outlook.com>
2020-10-19 20:38:32 +0100

    Create abstractclasses.php

---
+++ abstractclasses.php
@@ -0,0 +1,27 @@
+<?php
+abstract class Karate {
+  public $name;
+  public function __construct($name) {
+    $this->name = $name;
+  }
+}
+
+class Blocks extends Karate {
+    public function __construct($name);
+        $this->name;
+  }
+}
+
+class Kata extends Karate {
+    public function __construct($name);
+        $this->name!;
+  }
+}
+
+class Basics extends Karate {
+    public function __construct($name);
+        $this->name;
+  }
+}
+
+?>
