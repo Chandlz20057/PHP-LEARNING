446b131daa8a294caf9710ce323011cdf85ba8e7
Chandlz20057 <camchandlz@outlook.com>
2020-10-22 21:34:29 +0100

    Quick Task

---
+++ quicktask.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Quick Task:</title>
+</head>
+<body>
+    <marquee style="background-color:Red; width:1500px;border:5px solid black;">
+</div><h1><u>This is my Quick Self Set Task. Just a few random things that will include a mixture of php and HTML</u></h1></marquee>
+
+
+</body>
+</html>
