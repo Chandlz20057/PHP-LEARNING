7d35b7042cec3b54fc4b132f84d8e2f26a5fa25b
Chandlz20057 <camchandlz@outlook.com>
2020-10-24 23:22:18 +0100

    Update quicktask.php

--- quicktask.php
+++ quicktask.php
@@ -4,9 +4,15 @@
     <title>Quick Task:</title>
 </head>
 <body>
-    <marquee style="background-color:Red; width:1500px;border:5px solid black;">
+    <marquee style="background-color:Red;border:5px solid black;">
 <h1><u>This is my Quick Self Set Task. Just a few random things that will include a mixture of php and HTML</u></h1></marquee>
 
+<?php
+echo "<br />";
+echo "<h2>This is php code using the echo tag which prints out anything written in inverted commas (echo 'enter text here';)</h2>";
+echo "<br />";
+echo "<h2>Too create a line break you can either do echo out a br tag or write .PHP EOL after the echo coding. E.G. echo 'enter text here' . PHP EOL</h2>"
+?>
 
 </body>
 </html>
