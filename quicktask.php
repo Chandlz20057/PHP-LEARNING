474084f2f8a747433017dc188d026dfd363606c6
Ryan Chandler <ryangjchandler@gmail.com>
2020-10-24 23:24:43 +0100

    Update quicktask.php

--- quicktask.php
+++ quicktask.php
@@ -11,7 +11,7 @@
 echo "<br />";
 echo "<h2>This is php code using the echo tag which prints out anything written in inverted commas (echo 'enter text here';)</h2>";
 echo "<br />";
-echo "<h2>Too create a line break you can either do echo out a br tag or write .PHP EOL after the echo coding. E.G. echo 'enter text here' . PHP EOL</h2>"
+echo "<h2>Too create a line break you can either do echo out a &lt;br&gt; tag or write .PHP_EOL after the echo coding. E.G. echo 'enter text here' . PHP_EOL</h2>"
 ?>
 
 </body>
