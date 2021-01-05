dcbb0c28b4ce7187554bf8b1401ac478bcae8ee7
Chandlz20057 <camchandlz@outlook.com>
2021-01-05 19:13:27 +0000

    Create untitled.php

---
+++ untitled.php
@@ -0,0 +1,37 @@
+<?php
+$CarOptions= array(
+'Audi',
+'Lambo',
+'Volkswagen',
+'Mustang'
+);
+    $Car = array();
+    If(!empty($_POST['car']) && is_array($_POST['car'])){
+    Foreach($_POST['car'] as $Car){
+    If(in_array($Car,$CarOptions)){
+    $Car[]=$Car;
+    }
+    }
+    }
+    Var_dump($Car);
+    ?>
+    <form action=””method=”post”>
+    <label>
+    Audi
+    <input type=”checkbox” name=”vegetables[]” value=''>
+    <label>
+    <label>
+    Lambo
+    <input type=”checkbox” name=”vegetables[]” value=''>
+    </label>
+    <label>
+    Volkswagen
+    <input type=”checkbox” name=”vegetables[]” value=''>
+    </label>
+    <label>
+    Mustang
+    <input type=”checkbox” name=”vegetables[]” value=''>
+    </label>
+    <input type="submit" name="submit" value="true">
+    
+    </form>
